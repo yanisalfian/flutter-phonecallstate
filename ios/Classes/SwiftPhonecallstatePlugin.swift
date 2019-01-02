cb8067c757272ed5962b720b9618040439c14c6b
Yanis Alfian <alfian.fsi@outlook.com>
2019-01-02 11:38:27 +0700

    update init method in swift

--- ios/Classes/SwiftPhonecallstatePlugin.swift
+++ ios/Classes/SwiftPhonecallstatePlugin.swift
@@ -22,8 +22,8 @@ public class SwiftPhonecallstatePlugin: NSObject, FlutterPlugin, CXCallObserverD
     }
 
     init(channel:FlutterMethodChannel){
-        super.init()
         _channel = channel
+        super.init()
 
         setupCallObserver()
     }
